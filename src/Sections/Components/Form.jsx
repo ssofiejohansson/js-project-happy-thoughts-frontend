86c86d2b12964ed279d2b4d55fbb6d5991db230c
Sofie <sofieannamatilda@hotmail.se>
2025-05-01 15:09:13 +0200

    clean up

--- src/Sections/Components/Form.jsx
+++ src/Sections/Components/Form.jsx
@@ -2,10 +2,6 @@ import styled from "styled-components";
 import { SubmitButton } from "./Submit";
 
 export const Form = () => {
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> fab8659f6e1097bbd56965f0bbbb29a16b4a407e
 
   const TextArea = styled.textarea`
     width: 90%;
@@ -21,14 +17,7 @@ export const Form = () => {
       box-shadow: 0 0 5px #7a7b7b;
     }
   `;
- 
 
-<<<<<<< HEAD
-=======
-  const FormContainer = styled.div``;
->>>>>>> 5fcdef672e57fbf060c741a786ea501bcc8cdd17
-=======
->>>>>>> fab8659f6e1097bbd56965f0bbbb29a16b4a407e
   return (
     <form>
       <TextArea
